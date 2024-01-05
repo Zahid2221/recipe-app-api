4542de5dc6b5ff269381306d452e5bc35f20247c
Zahid <mhdzahid269@gmail.com>
2024-01-05 21:33:06 +0530

    added changes in calc and tests

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,11 +2,13 @@
 Calculator Functionality
 '''
 
-def add(x,y):
+
+def add(x, y):
+
     '''Add x and y'''
     return x+y
 
-def sub(x,y):
+def sub(x, y):
     '''Substract x and y'''
 
     return y-x
